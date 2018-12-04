3946fb0ba1846ba7a7a1bd835108fbf2e9e464cc
Chris Smeele <cj.smeele@xs4all.nl>
2018-12-04 20:14:31 +0100

    day01: use Set for better performance

--- week48/day01.hs
+++ week48/day01.hs
@@ -1,12 +1,13 @@
 module Main where
 
-import Common
+import           Common
+import qualified Data.Set as S
 
 work = sum
 
-work' = f [] 0 . cycle
-    where f acc n (x:xs) | n `elem` acc = n
-                         | otherwise    = f (n:acc) (n+x) xs
+work' = f S.empty 0 . cycle
+    where f acc n (x:xs) | n `S.member` acc = n
+                         | otherwise        = f (S.insert n acc) (n+x) xs
 
 p :: Parser Char [Int]
 p = some (line $=> integer) <* end
